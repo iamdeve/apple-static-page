9c6c32cc295ba929f975fc638125bc92b4113ba3
imwebdeve <wajdanaeli@gmail.com>
2020-10-01 14:52:52 +0500

    change link

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
 const followLink = () => {
-	window.location.href = 'http://blatures-rachile.com/click';
+	// window.location.href = 'http://blatures-rachile.com/click';
+	window.location.href = 'https://bit.ly/3iehaGd';
 };
